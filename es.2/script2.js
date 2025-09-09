33e0dd4aff0f38254d65878d5a29c8d9c2d6b361
LuigiCasale95 <luigicasale95@gmail.com>
2025-09-09 16:21:16 +0200

    Completamento secondo esercizio

--- es.2/script2.js
+++ es.2/script2.js
@@ -5,3 +5,45 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
  */
+
+/* Creaziione array di oggetti */
+const squadre = [
+
+    {nome : "Milan", punti : 0, falliSub : 0 },
+    {nome : "Napoli", punti : 0, falliSub : 0 },
+    {nome : "Lazio", punti : 0, falliSub : 0 },
+    {nome : "Roma", punti : 0, falliSub : 0 },
+    {nome : "Inter", punti : 0, falliSub : 0 },
+    {nome : "Fiorentina", punti : 0, falliSub : 0 },
+
+];
+console.log(squadre)
+
+for (i = 0; i < squadre.length; i++) {
+    squadre[i].punti = randomNumber(1,10,1)
+    squadre[i].falliSub = randomNumber(1,10,1)
+}
+
+/* Array Vuoto */
+const falliSubSquadre = []
+
+for (i = 0; i < squadre.length; i++) {
+    falliSubSquadre.push(squadre[i].nome, squadre[i].falliSub)
+}
+
+console.log(falliSubSquadre)
+
+
+
+
+
+
+
+
+
+/* FUNZIONI */
+/* Generatore numeri casuali tra min e max*/
+function randomNumber(min, max) {
+    let number = Math.floor(Math.random() * (max - min +1)) + min
+    return number
+}
