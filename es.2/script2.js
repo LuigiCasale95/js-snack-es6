c32acee10531c899602df2c08c54a55b41f62558
LuigiCasale95 <luigicasale95@gmail.com>
2025-09-09 21:10:54 +0200

    Aggiunta stampa in pagina delle 2 liste

--- es.2/script2.js
+++ es.2/script2.js
@@ -5,7 +5,8 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
  */
-
+const liSquadre = document.querySelector(".listaSquadre")
+const liSquadre2 = document.querySelector(".listaSquadre2")
 /* Creaziione array di oggetti */
 const squadre = [
 
@@ -38,7 +39,10 @@ console.log(falliSubSquadre)
 
 
 
-
+for (i = 0; i<squadre.length; i++) {
+    liSquadre.innerHTML += `<li>${squadre[i].nome}-- Punti= ${squadre[i].punti}-- Falli subbiti=${squadre[i].falliSub}</li>`
+    liSquadre2.innerHTML += `<li>${squadre[i].nome}-- Falli subbiti=${squadre[i].falliSub}</li>`
+}
 
 
 /* FUNZIONI */
